f51962dd11d0810592bc2eef298d06ca3523ab00
Femi Ajayi <femiajayi705@gmail.com>
2023-01-03 23:36:46 +0100

    Read blog data from server

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Link from "next/link";
 
-const BlogPosts = () => {
-  const blog_posts = [{ id: 1, title: "First blog post" }];
+const BlogPosts = ({ blogPosts }) => {
+  const blog_posts = blogPosts.length
+    ? blogPosts
+    : [{ id: 1, title: "First blog post" }];
   return (
     <div>
       <h1>Blog</h1>
@@ -17,5 +19,22 @@ const BlogPosts = () => {
     </div>
   );
 };
+
+export const returnBaseUrl = () => {
+  let base_url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://lionfish-app-aooyz.ondigitalocean.app"; // https://v2ds.netlify.app
+
+  return base_url;
+};
+
+export async function getStaticProps() {
+  const res = await fetch(`${returnBaseUrl()}/api/blog_posts`);
+  const { blogPosts } = await res.json();
+  return {
+    props: { blogPosts },
+  };
+}
 
 export default BlogPosts;
