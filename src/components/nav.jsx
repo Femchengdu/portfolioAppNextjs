f00f0320bfa6d2fe2c4086ca5a02b605d78d8fcf
Femi Ajayi <femiajayi705@gmail.com>
2023-01-03 22:14:41 +0100

    feat: Updated navigation links

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,23 +6,23 @@ const Nav = () => (
         <ul>
             <li>
             <h1>
-                <Link href="index.html">
+                <Link href="/">
                 <span className="fas fa-code" aria-hidden="true"></span>
                 Freenovation &nbsp;<span role="img" aria-label="hacker">💻</span>
                 </Link>
             </h1>
             </li>
-            <li><Link href="#projects">Projects</Link></li>
-            <li><Link href="about.html">About</Link></li>
-            <li><Link href="#contact">Contact</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/blog">Blog</Link></li>
+            <li><Link href="about.html">Projects</Link></li>
             <li>
-            <Link href="https://www.linkedin.com/in/jen4web/" target="_blank">
+            <Link href="https://www.linkedin.com/in/samuel-ajayi-full-stack/" target="_blank">
                 <span className="fab fa-linkedin" aria-hidden="true"></span>
                 <span className="sr-only">LinkedIn</span>
             </Link>
             </li>
             <li>
-            <Link href="https://www.github.com/jen4web" target="_blank">
+            <Link href="https://github.com/Femchengdu" target="_blank">
                 <span className="fab fa-github-square" aria-hidden="true"></span>
                 <span className="sr-only">Github</span>
             </Link>
