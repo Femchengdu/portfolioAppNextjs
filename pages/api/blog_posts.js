443eaf6e0443a3eb139f801ccc67424ebecc5d61
Femi Ajayi <femiajayi705@gmail.com>
2023-01-04 21:26:57 +0100

    feat: Removed unused code from blog_post api route

--- pages/api/blog_posts.js
+++ pages/api/blog_posts.js
@@ -1,44 +1,6 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import remark from "remark";
-import html from "remark-html";
-// Ther reference is the docker home /app/portfolio
-const postsDirectory = path.join(process.cwd(), "blog_posts");
-
-const getSortedPostsData = () => {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
-
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data,
-    };
-  });
-  // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-};
+import { getSortedPostsData } from "../../utils/blog";
 
 export default function handler(req, res) {
   const blogPosts = getSortedPostsData();
-
   res.status(200).json({ blogPosts });
 }
